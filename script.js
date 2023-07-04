6348889e5bcf0f106e198bddd6bb66d27e3341f2
ifayyad <fayyadan@gmail.com>
2023-07-05 00:19:14 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false, // Remove navigation arrows
+        pauseOnHover: false, // Prevent pausing on hover
+        pauseOnFocus: false // Prevent pausing on focus
+    });
+});
