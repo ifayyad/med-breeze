164c87a4e4b55df512667b0e1e4dc38c0c08f6e8
ifayyad <fayyadan@gmail.com>
2023-07-11 00:53:14 +0200

    code organization

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-$(document).ready(function(){
-    $('.carousel').slick({
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        arrows: true, // Remove navigation arrows
-        pauseOnHover: false, // Prevent pausing on hover
-        pauseOnFocus: false // Prevent pausing on focus
-    });
+$(document).ready(function () {
+  $(".carousel").slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: true, // Remove navigation arrows
+    pauseOnHover: false, // Prevent pausing on hover
+    pauseOnFocus: false, // Prevent pausing on focus
+  });
 });
 
-
 // Get all menu items
-const menuItems = document.querySelectorAll('.menu-item');
+const menuItems = document.querySelectorAll(".menu-item");
 
 // Add click event listener to each menu item
-menuItems.forEach(function(item) {
-  item.addEventListener('click', function() {
-    const description = item.querySelector('.description');
-    description.classList.toggle('show');
+menuItems.forEach(function (item) {
+  item.addEventListener("click", function () {
+    const description = item.querySelector(".description");
+    description.classList.toggle("show");
   });
 });
